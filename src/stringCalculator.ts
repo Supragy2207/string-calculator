4afc7c8f6ec9c21a37432f01e70f2a98687d9cfd
Supragya <Supragy2207@gmail.com>
2025-10-08 00:37:52 +0530

    single number returns same number

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,6 @@
 export function add(numbers: string): number {
-    if (numbers === '') return 0;
-    return 0; 
-  }
-  
+  if (numbers === "") return 0; // empty string returning 0
+
+  const num = Number(numbers);
+  return isNaN(num) ? 0 : num; //single number returns same number
+}
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,5 +1,9 @@
-import { add } from '../src/stringCalculator';
+import { add } from "../src/stringCalculator";
 
-test('returns 0 for an empty string', () => {
-  expect(add('')).toBe(0);
+test("returns 0 for an empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test("returns number itself for single input", () => {
+  expect(add("5")).toBe(5);
 });
